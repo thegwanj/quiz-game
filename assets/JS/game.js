694ebf8ecf3c9654f545a681a0ca722ad9966752
thegwanj <gwanjuchung@gmail.com>
2022-08-23 16:26:33 -0700

    Declared timerEl variable

---
+++ assets/JS/game.js
@@ -0,0 +1,19 @@
+// DECLARE 'countdown' variable
+
+// DECLARE 'questions' array
+
+var questions = [
+    {
+        //question
+        //answers
+        //correct answers
+    },
+    {
+        //question
+        //answers
+        //correct answers
+    }
+];
+
+// DECLARE the 'timerEl'
+var timerEl = document.querySelector('#timer');
