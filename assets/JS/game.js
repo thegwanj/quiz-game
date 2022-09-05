b6558f64e40ffdaff770eafe46a7cb867168b309
unknown <gwanjuchung@yahoo.com>
2022-09-05 16:23:01 -0700

    Added in pseudocode from demo

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -17,3 +17,25 @@ var questions = [
 
 // DECLARE the 'timerEl'
 var timerEl = document.querySelector('#timer');
+
+// Function startGame
+
+    // Hide the start screen
+
+    // Display the first question
+
+    // Set the starting value of 'countdown'
+
+    // Start the timer 'startTimer'
+
+// Function 'answerQuestion'
+
+    // Check if the selected answer is correct
+
+        // IF the answer is wrong
+
+            // THEN we need to subtract from 'countdown'
+
+    // Display the next question
+
+// Function 'displayCurrentQuestion'
