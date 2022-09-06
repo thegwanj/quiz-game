193f50f38458f57ba7f02ae2324b7d2a06238e23
unknown <gwanjuchung@yahoo.com>
2022-09-05 17:00:18 -0700

    Added in additional code from demo

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -1,11 +1,12 @@
 // DECLARE 'countdown' variable
 
 // DECLARE 'questions' array
-
+var questionPosition = 0;
 var questions = [
     {
         //question
         //answers
+        answers: ["Answer A", "Answer B"]
         //correct answers
     },
     {
@@ -15,20 +16,39 @@ var questions = [
     }
 ];
 
+// DECLARE a 'timerInterval'
+
+var currentQuestion = questions[questionPosition];
+
 // DECLARE the 'timerEl'
 var timerEl = document.querySelector('#timer');
 
 // Function startGame
-
+function startGame() {
     // Hide the start screen
 
-    // Display the first question
+    // Set the question position to 0
+
+    // Display the first question (Display the current question)
 
     // Set the starting value of 'countdown'
 
     // Start the timer 'startTimer'
 
+        // IF 'countdown' === 0 THEN 'endGame()'
+}
+    
+
+// Function 'endGame'
+function endGame() {
+    // Hide the questions area
+
+    // Show the record high score
+}
+
 // Function 'answerQuestion'
+function answerQuestion() {
+    //var currentQuestion = questions[questionPosition];
 
     // Check if the selected answer is correct
 
@@ -36,6 +56,25 @@ var timerEl = document.querySelector('#timer');
 
             // THEN we need to subtract from 'countdown'
 
-    // Display the next question
+    //Increasing the question position by 1
+
+    // IF I've passed the last question
+
+        // THEN  'endGame()'
+
+    // ELSE
+    
+        // Display the current question
+
+        // questionPosition++;
+        // displayCurrentQuestion();
+}
+    
 
 // Function 'displayCurrentQuestion'
+function displayCurrentQuestion() {
+    //questions[questionPosition];
+
+}
+
+// Function 'recordHighScore'
