222b9dbb58226116296b228c20a772f8ab484bf8
thegwanj <gwanjuchung@gmail.com>
2022-09-06 15:23:54 -0700

    Added in template for doing timer

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -1,4 +1,5 @@
 // DECLARE 'countdown' variable
+var countdown = 0;
 
 // DECLARE 'questions' array
 var questionPosition = 0;
@@ -17,6 +18,32 @@ var questions = [
 ];
 
 // DECLARE a 'timerInterval'
+// function startTimer() {
+//     var timeLeft = 5;
+  
+//     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+//     var timeInterval = setInterval(function () {
+//       // As long as the `timeLeft` is greater than 1
+//       if (timeLeft > 1) {
+//         // Set the `textContent` of `timerEl` to show the remaining seconds
+//         timerEl.textContent = timeLeft + ' seconds remaining';
+//         // Decrement `timeLeft` by 1
+//         timeLeft--;
+//       } else if (timeLeft === 1) {
+//         // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+//         timerEl.textContent = timeLeft + ' second remaining';
+//         timeLeft--;
+//       } else {
+//         // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+//         timerEl.textContent = '';
+//         // Use `clearInterval()` to stop the timer
+//         clearInterval(timeInterval);
+//         // Call the `displayMessage()` function
+//         displayMessage();
+//       }
+//     }, 1000);
+//   }
+  
 
 var currentQuestion = questions[questionPosition];
 
@@ -28,10 +55,12 @@ function startGame() {
     // Hide the start screen
 
     // Set the question position to 0
+    questionPosition = 0;
 
     // Display the first question (Display the current question)
 
     // Set the starting value of 'countdown'
+    countdown = 10;
 
     // Start the timer 'startTimer'
 
