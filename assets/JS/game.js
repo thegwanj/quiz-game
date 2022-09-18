634db0252525039edd033fc3e2c4cb139249d3a6
unknown <gwanjuchung@yahoo.com>
2022-09-17 19:34:54 -0700

    Implemented the startTimer function

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -97,31 +97,28 @@ var questions = [
 // DECLARE a 'timerInterval'
 var timeInterval;
 
-// function startTimer() {
-//     var timeLeft = 5;
-  
-//     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-//     timeInterval = setInterval(function () {
-//       // As long as the `timeLeft` is greater than 1
-//       if (timeLeft > 1) {
-//         // Set the `textContent` of `timerEl` to show the remaining seconds
-//         timerEl.textContent = timeLeft + ' seconds remaining';
-//         // Decrement `timeLeft` by 1
-//         timeLeft--;
-//       } else if (timeLeft === 1) {
-//         // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-//         timerEl.textContent = timeLeft + ' second remaining';
-//         timeLeft--;
-//       } else {
-//         // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-//         timerEl.textContent = '';
-//         // Use `clearInterval()` to stop the timer
-//         clearInterval(timeInterval);
-//         // Call the `displayMessage()` function
-//         displayMessage();
-//       }
-//     }, 1000);
-//   }
+function startTimer(timeLeft) {  
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    timeInterval = setInterval(function () {
+      // As long as the `timeLeft` is greater than 1
+      if (timeLeft > 1) {
+        // Set the `textContent` of `timerEl` to show the remaining seconds
+        timerEl.textContent = timeLeft;
+        // Decrement `timeLeft` by 1
+        timeLeft--;
+      } else if (timeLeft === 1) {
+        // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+        timerEl.textContent = timeLeft;
+        timeLeft--;
+      } else {
+        timerEl.textContent = '0';
+        // Use `clearInterval()` to stop the timer
+        clearInterval(timeInterval);
+        // Call the `answerQuestion` function to go to next question
+        answerQuestion();
+      }
+    }, 1000);
+  }
   
 
 var currentQuestion = questions[questionPosition];
@@ -142,7 +139,7 @@ function startGame() {
     countdown = 10;
 
     // Start the timer 'startTimer'
-    startTimer();
+    startTimer(countdown);
         // IF 'countdown' === 0 THEN 'endGame()'
 }
     
